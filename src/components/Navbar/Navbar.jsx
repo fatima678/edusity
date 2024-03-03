d7a69f44a346a712f41d32b926ab89bc8c6e79c8
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-03-03 15:00:17 +0500

    Initial commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Navbar.css'
+import logo  from '../../assets/logo.png'
+
+const Navbar = () => {
+  return (
+    <nav>
+        <img src={logo}></img>
+        <ul>
+            <li>Home</li>
+            <li>Program</li>
+            <li>About us</li>
+            <li>Campus</li>
+            <li>Testimonials</li>
+            <li>Contact us</li>
+        </ul>
+
+    </nav>
+  )
+}
+
+export default Navbar
