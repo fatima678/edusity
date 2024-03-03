b4c7bb4e4f9b4d7eadb57737c3927b6f8becce68
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-03-03 16:09:52 +0500

    Edusity website using react

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero container'>
+        <div className="hero-txt">
+            <h2>We Ensure better education for better world</h2>
+            <p>Our cutting-edge  curriculum  is designed to empower students with the knowledge, skills,  and 
+                experiences  needed to excel  in the dynamic  field of education
+            </p>
+            <button className='btn'>Explore more</button>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Hero
