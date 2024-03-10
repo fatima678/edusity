285a8707ff686d091e48036b92105c0df4d1fd52
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-03-10 16:19:05 +0500

    Design the contact us component with jsx and css

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Title from './components/Title/Title'
 import About from './components/About/About'
 import Campus from './components/Campus/Campus'
 import Testimonial from './components/Testimonials/Testimonial'
+import Contact from './components/Contact/Contact'
 
 const App = () => {
   return (
@@ -20,6 +21,8 @@ const App = () => {
       <Campus/>
       <Title subTitle='TESTIMONIALS' title='What Students Says'/>
       <Testimonial/>
+      <Title subTitle='Contact Us' title='Get in Touch'/>
+      <Contact/>
       </div>
      
     </div>
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Contact.css'
+
+const Contact = () => {
+  return (
+    <div className='contact'>
+        <div className="contact-col">
+            <h3>Send us a message</h3>
+            <p>Feel free to contact us for any query , your suggestions and feedback are very
+                important for us
+            </p>
+            <ul>
+                <li>abc@gmail.com</li>
+                <li>03000000000</li>
+                <li>Gujranwala, main road</li>
+            </ul>
+        </div>
+        <div className="contact-col">
+
+        </div>
+      
+    </div>
+  )
+}
+
+export default Contact
