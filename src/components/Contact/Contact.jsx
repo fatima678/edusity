fdf573167a66b361a7f0f0721c051250fbd4d2b5
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-03-12 15:39:54 +0500

    Email integration with frontend through API

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -6,6 +6,34 @@ import phone_icon from '../../assets/phone-icon.png'
 import location_icon from '../../assets/location-icon.png'
 import white_arrow from '../../assets/white-arrow.png'
 const Contact = () => {
+    const [result, setResult] = React.useState("");
+
+    const onSubmit = async (event) => {
+      event.preventDefault();
+      setResult("Sending....");
+      const formData = new FormData(event.target);
+  
+      formData.append("access_key", "1c04df8b-f8f7-49e4-be40-7db82d141c40");
+  
+      const response = await fetch("https://api.web3forms.com/submit", {
+        method: "POST",
+        body: formData
+      });
+  
+      const data = await response.json();
+  
+      if (data.success) {
+        setResult("Form Submitted Successfully");
+        event.target.reset();
+      } else {
+        console.log("Error", data);
+        setResult(data.message);
+      }
+    };
+
+
+
+
   return (
     <div className='contact'>
         <div className="contact-col">
@@ -20,7 +48,7 @@ const Contact = () => {
             </ul>
         </div>
         <div className="contact-col">
-            <form>
+            <form onSubmit={onSubmit}>  
                 <label>Your name</label>
                 <input type='text' name='name' placeholder='Enter your name' required></input>
                 <label>Phone Number</label>
@@ -30,7 +58,7 @@ const Contact = () => {
                 <button type='submit'   className='btn dark-btn'>Submit now<img src={white_arrow} alt=''></img></button>
 
             </form>
-            <span></span>
+            <span>{result}</span>
 
         </div>
       
