7c7e2514d8c58f895c58f34a73ac918fd2d7288d
fatima678 <119948111+fatima678@users.noreply.github.com>
2024-03-19 23:57:18 +0500

    Implement the react scroll

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,12 +7,12 @@ const Navbar = () => {
     <nav className='container'>
         <img src={logo} className='logo'></img>
         <ul>
-            <li>Home</li>
-            <li>Program</li>
-            <li>About us</li>
-            <li>Campus</li>
-            <li>Testimonials</li>
-            <li><button className='btn'>Contact us</button></li>
+            <li><Link to='Home' smooth={true} offdet={0} duration={500}>Home</Link></li>
+            <li><Link to='program' smooth={true} offdet={0} duration={500}>Program</Link></li>
+            <li><Link to='about us' smooth={true} offdet={0} duration={500}>About us</Link></li>
+            <li><Link to='campus' smooth={true} offdet={0} duration={500}>Campus</Link></li>
+            <li><Link to='testimonials' smooth={true} offdet={0} duration={500}>Testimonials</Link></li>
+            <li><Link to='contact' smooth={true} offdet={0} duration={500} className='btn'>Contact us</Link></li>
         </ul>
 
     </nav>
